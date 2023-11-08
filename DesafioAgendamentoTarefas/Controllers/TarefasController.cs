1f42d9fdeb37a2c053aa0c4f093e5c149a4558ca
Wendell Morais <wendellmorais.dev@gmail.com>
2023-11-08 11:43:37 -0300

    obter tarefa por ID

--- DesafioAgendamentoTarefas/Controllers/TarefasController.cs
+++ DesafioAgendamentoTarefas/Controllers/TarefasController.cs
@@ -18,5 +18,15 @@ namespace DesafioAgendamentoTarefas.Controllers
         {
             _context = context;
         }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            var tarefa = _context.Tarefas.Find(id);
+           
+            if (tarefa == null) return NotFound();
+            
+            return Ok(tarefa);
+        }
     }
 }
