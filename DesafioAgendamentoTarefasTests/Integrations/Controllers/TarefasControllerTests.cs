690c98666065f13ca2364e1de12f7d04133a4742
Wendell Morais <wendellmorais.dev@gmail.com>
2023-11-09 09:35:59 -0300

    tests: setup inicial

---
+++ DesafioAgendamentoTarefasTests/Integrations/Controllers/TarefasControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesafioAgendamentoTarefasTests.Integrations.Controllers
+{
+    public class TarefasControllerTests
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        private readonly HttpClient _httpClient;
+
+        public TarefasControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+            _httpClient = _factory.CreateClient();
+        }
+    }
+}
