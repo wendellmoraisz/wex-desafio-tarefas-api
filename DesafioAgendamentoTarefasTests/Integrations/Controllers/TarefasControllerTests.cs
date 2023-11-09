385087dea85fc1b14496592d29766fdd6d5e3d5b
Wendell Morais <wendellmorais.dev@gmail.com>
2023-11-09 10:55:42 -0300

    tests: criar tarefa

--- DesafioAgendamentoTarefasTests/Integrations/Controllers/TarefasControllerTests.cs
+++ DesafioAgendamentoTarefasTests/Integrations/Controllers/TarefasControllerTests.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using DesafioAgendamentoTarefas.Controllers;
+using DesafioAgendamentoTarefas.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 
 namespace DesafioAgendamentoTarefasTests.Integrations.Controllers
 {
-    public class TarefasControllerTests
+    public class TarefasControllerTests : IClassFixture<WebApplicationFactory<Program>>
     {
         private readonly WebApplicationFactory<Program> _factory;
         private readonly HttpClient _httpClient;
@@ -16,6 +20,25 @@ namespace DesafioAgendamentoTarefasTests.Integrations.Controllers
         {
             _factory = factory;
             _httpClient = _factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task CriarTarefaDeveRetornarSucesso()
+        {
+            var tarefaModel = new Tarefa
+            {
+                Data = DateTime.Now,
+                Titulo = "Estudar C#",
+                Descricao = "Assistir as aulas do Bootcamp Wex",
+                Status = EStatusTarefa.Pendente
+            };
+
+            StringContent content = new StringContent(JsonConvert.SerializeObject(tarefaModel), Encoding.UTF8, "application/json");
+
+            var httpResponse = await _httpClient.PostAsync("/Tarefa", content);
+
+            Assert.NotNull(httpResponse.Content);
+            Assert.Equal(HttpStatusCode.Created, httpResponse.StatusCode);
         }
     }
 }
